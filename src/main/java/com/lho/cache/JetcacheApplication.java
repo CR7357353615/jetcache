ba95fb53412feb122099359bd42312e97418de70
CR7357353615 <357353615@qq.com>
2018-09-20 12:23:10 +0800

    init

---
+++ src/main/java/com/lho/cache/JetcacheApplication.java
@@ -0,0 +1,12 @@
+package com.lho.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetcacheApplication.class, args);
+	}
+}
